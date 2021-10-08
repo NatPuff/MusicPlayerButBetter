e07ad23556ffc16ee7c7b619743471b5cd455c3d
NatPuff <cesarp102104@gmail.com>
2021-10-08 17:48:07 -0500

    Initial Commit

---
+++ MusicPlayerButBetter/MusicPlayerButBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerButBetterApp.swift
+//  MusicPlayerButBetter
+//
+//  Created by Cesar Pacheco on 10/8/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerButBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
